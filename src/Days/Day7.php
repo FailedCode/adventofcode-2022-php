51567b1c76b67dd9c8c2dcaf38dcce03989c6941
FailedCode <xeres666@googlemail.com>
2022-12-09 23:03:27 +0100

    Day 7, part1

---
+++ src/Days/Day7.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Failedcode\Aoc2022\Days;
+
+class Day7 extends AbstractDay
+{
+    public function solve_part_1(): string
+    {
+        $filesystem = $this->getFilesystem();
+        $dirSizes = [];
+        $totalSize = 0;
+        foreach ($filesystem as $path => $size) {
+            if (str_ends_with($path, '/')) {
+                continue;
+            }
+
+            while (true) {
+                $path = dirname($path);
+                if (!isset($dirSizes[$path])) {
+                    $dirSizes[$path] = 0;
+                }
+                $dirSizes[$path] += $size;
+                if ($path === '/') {
+                    break;
+                }
+            }
+
+        }
+
+        $maxSize = 100000;
+        foreach ($dirSizes as $dir => $size) {
+            if ($size <= $maxSize) {
+                $totalSize += $size;
+            }
+        }
+
+        return $totalSize;
+    }
+
+    public function solve_part_2(): string
+    {
+        return "TODO";
+    }
+
+    protected function getFilesystem()
+    {
+        $sep = '/';
+        $currentDir = $sep;
+        $filesystem[$currentDir] = [];
+        $inputList = $this->util->loadInput(7);
+        foreach ($inputList as $row) {
+            if (preg_match('~\$ cd (\S+)~', $row, $match)) {
+                $dir = trim($match[1]);
+                if ($dir === '..') {
+                    $currentDir = $this->backOneDir($currentDir, $sep);
+                    continue;
+                }
+                if ($dir === '/') {
+                    $currentDir = $sep;
+                    continue;
+                }
+                $currentDir .= "{$dir}$sep";
+                if (!isset($filesystem[$currentDir])) {
+                    $filesystem[$currentDir] = [];
+                }
+                continue;
+            }
+            if ($row === '$ ls') {
+                continue;
+            }
+            if (preg_match('~dir (\S+)~', $row, $match)) {
+                $dir = $currentDir . "{$match[1]}$sep";
+                if (!isset($filesystem[$dir])) {
+                    $filesystem[$dir] = [];
+                }
+                continue;
+            }
+            if (preg_match('~(\d+) (\S+)~', $row, $match)) {
+                $file = $currentDir . "{$match[2]}";
+                $filesystem[$file] = (int)$match[1];
+            }
+        }
+        return $filesystem;
+    }
+
+    protected function backOneDir($dir, $sep)
+    {
+        $parts = explode($sep, $dir);
+        array_pop($parts);
+        array_pop($parts);
+        return implode($sep, $parts) . $sep;
+    }
+}
